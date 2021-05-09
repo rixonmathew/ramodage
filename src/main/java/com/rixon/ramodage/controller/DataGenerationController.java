c004b61c8e2b5afb160bf842fb6bf6a46a9df8a3
rixonmathew <rixonmathew@gmail.com>
2021-05-09 21:21:20 +0530

    WIP changes

---
+++ src/main/java/com/rixon/ramodage/controller/DataGenerationController.java
@@ -0,0 +1,14 @@
+package com.rixon.ramodage.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataGenerationController {
+
+    public void generateData() {
+        /*
+          Load the options, read from request, generate data and provide response
+          For async mode return a request id and provide endpoint to stream data
+         */
+    }
+}
